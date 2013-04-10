88b2715ecf04ee318293bc399308192159e96507
mmartin <mmartin@mezeo.com>
2013-04-10 16:00:35 -0500

    Change variable name 'filter' (reserved) to 'filtr'.

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -137,15 +137,15 @@ class TestVaryFilter(unittest.TestCase):
     def test_addHeader(self):
         options = CorsOptions(vary=True)
         response = CorsResponse()
-        filter = filters.VaryFilter(options)
-        filter.filter(None, response)
+        filtr = filters.VaryFilter(options)
+        filtr.filter(None, response)
         self.assertEquals('Origin', response.headers['Vary'])
 
     def test_noHeader(self):
         options = CorsOptions(vary=False)
         response = CorsResponse()
-        filter = filters.VaryFilter(options)
-        filter.filter(None, response)
+        filtr = filters.VaryFilter(options)
+        filtr.filter(None, response)
         self.assertFalse('Vary' in response.headers)
 
 
